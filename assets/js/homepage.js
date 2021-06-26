69102f7e1c4c8cf092880e3e9b0a59e6fd61d71a
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-06-26 16:00:00 -0700

    form submission complete

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,15 +1,30 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter your GitHub username")
+    }
+}
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    // make a call to the api 
+    // make a call to the api
+    // fetch('').then(response).then(data)
+
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
-getUserRepos("facebook");
-
-// try bwogi facebook microsoft etc. 
-
-// fetch('').then(response).then(data)
+// try bwogi ,facebook,microsoft etc. 
+// getUserRepos("facebook");
+userFormEl.addEventListener("submit", formSubmitHandler)
